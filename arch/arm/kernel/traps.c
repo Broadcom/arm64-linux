19bcd0015dc206157ef2a5f9232b93db14a8474e
Russell King <rmk+kernel@arm.linux.org.uk>
2014-08-18 10:28:51 +0100

    ARM: cobble together FIQ backtracing
    Cobble the FIQ backtracing together, some of this patch is based on
    some of Daniel Thompson's work.  Experimental, and not for submission
    yet.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/kernel/traps.c
+++ arch/arm/kernel/traps.c
@@ -26,6 +26,7 @@
 #include <linux/init.h>
 #include <linux/sched.h>
 #include <linux/irq.h>
+#include <linux/nmi.h>
 
 #include <linux/atomic.h>
 #include <asm/cacheflush.h>
@@ -480,6 +481,9 @@ asmlinkage void __exception_irq_entry handle_fiq_as_nmi(struct pt_regs *regs)
 	nmi_enter();
 
 	/* nop. FIQ handlers for special arch/arm features can be added here. */
+#ifdef CONFIG_SMP
+	nmi_cpu_backtrace(regs);
+#endif
 
 	nmi_exit();
 
