dec6eb6c3ded6e20fd3ddd1680c31fc96babfdec
Ray Jui <rjui@broadcom.com>
2015-07-10 17:43:40 -0700

    VOIP-4918: Add iProc PCIe outbound mapping support
    PCI: iproc: Add outbound mapping configuration support
    
    Certain iProc SoCs require the PCIe outbound mapping to be configured in
    SW. This patch adds support for those chips
    
    Signed-off-by: Ray Jui <rjui@broadcom.com>
    Bug: VOIP-4918
    Change-Id: Ia314eae230e2bae3f648960cd0f283184ff35224

--- drivers/pci/host/pcie-iproc.h
+++ drivers/pci/host/pcie-iproc.h
@@ -17,6 +17,19 @@
 #define IPROC_PCIE_MAX_NUM_IRQS 6
 
 /**
+ * iProc PCIe outbound mapping
+ * @set_oarr_size: indicates the OARR size bit needs to be set
+ * @axi_offset: offset from the AXI address to the internal address used by
+ * the iProc PCIe core
+ * @window_size: outbound window size
+ */
+struct iproc_pcie_ob {
+	bool set_oarr_size;
+	resource_size_t axi_offset;
+	resource_size_t window_size;
+};
+
+/**
  * iProc PCIe device
  * @dev: pointer to device data structure
  * @base: PCIe host controller I/O register base
@@ -25,6 +38,8 @@
  * @root_bus: pointer to root bus
  * @phy: optional PHY device that controls the Serdes
  * @irqs: interrupt IDs
+ * @need_ob_cfg: indicates SW needs to configure the outbound mapping window
+ * @ob: outbound mapping parameters
  */
 struct iproc_pcie {
 	struct device *dev;
@@ -36,6 +51,8 @@ struct iproc_pcie {
 	struct pci_bus *root_bus;
 	struct phy *phy;
 	int irqs[IPROC_PCIE_MAX_NUM_IRQS];
+	bool need_ob_cfg;
+	struct iproc_pcie_ob ob;
 };
 
 int iproc_pcie_setup(struct iproc_pcie *pcie);
