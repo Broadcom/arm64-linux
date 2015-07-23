99f631e226bf2b5e95e9172925d0adfb4a52bb6f
Andrea Arcangeli <aarcange@redhat.com>
2015-07-24 09:10:53 +1000

    userfaultfd-add-new-syscall-to-provide-memory-externalization-fix
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/userfaultfd.c
+++ fs/userfaultfd.c
@@ -478,6 +478,21 @@ static void __wake_userfault(struct userfaultfd_ctx *ctx,
 static __always_inline void wake_userfault(struct userfaultfd_ctx *ctx,
 					   struct userfaultfd_wake_range *range)
 {
+	/*
+	 * To be sure waitqueue_active() is not reordered by the CPU
+	 * before the pagetable update, use an explicit SMP memory
+	 * barrier here. PT lock release or up_read(mmap_sem) still
+	 * have release semantics that can allow the
+	 * waitqueue_active() to be reordered before the pte update.
+	 */
+	smp_mb();
+
+	/*
+	 * Use waitqueue_active because it's very frequent to
+	 * change the address space atomically even if there are no
+	 * userfaults yet. So we take the spinlock only when we're
+	 * sure we've userfaults to wake.
+	 */
 	if (waitqueue_active(&ctx->fault_wqh))
 		__wake_userfault(ctx, range);
 }
