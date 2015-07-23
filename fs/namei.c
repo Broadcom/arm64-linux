f5d34f02184e9f7ee8a3de0f229788779300ccd5
Masanari Iida <standby24x7@gmail.com>
2015-07-24 09:20:29 +1000

    namei: fix warning while make xmldocs caused by namei.c
    Fix the following warnings:
    
    Warning(.//fs/namei.c:2422): No description found for parameter 'nd'
    Warning(.//fs/namei.c:2422): Excess function parameter 'nameidata'
    description in 'path_mountpoint'
    
    Signed-off-by: Masanari Iida <standby24x7@gmail.com>
    Acked-by: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/namei.c
+++ fs/namei.c
@@ -2484,7 +2484,7 @@ done:
 
 /**
  * path_mountpoint - look up a path to be umounted
- * @nameidata:	lookup context
+ * @nd:		lookup context
  * @flags:	lookup flags
  * @path:	pointer to container for result
  *
