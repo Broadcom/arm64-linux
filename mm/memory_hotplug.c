bb9655c6059a10c4dd11edc3521f8683d3e8c38d
James Custer <jcuster@sgi.com>
2015-07-24 09:11:34 +1000

    mm: fix invalid use of pfn_valid_within in test_pages_in_a_zone
    Offlining memory by 'echo 0 > /sys/devices/system/memory/memory#/online'
    or reading valid_zones 'cat
    /sys/devices/system/memory/memory#/valid_zones' causes BUG: unable to
    handle kernel paging request due to invalid use of pfn_valid_within.  This
    is due to a bug in test_pages_in_a_zone.
    
    In order to use pfn_valid_within within a MAX_ORDER_NR_PAGES block of
    pages, a valid pfn within the block must first be found.  There only needs
    to be one valid pfn found in test_pages_in_a_zone in the first place.  So
    the fix is to replace pfn_valid_within with pfn_valid such that the first
    valid pfn within the pageblock is found (if it exists).  This works
    independently of CONFIG_HOLES_IN_ZONE.
    
    Signed-off-by: James Custer <jcuster@sgi.com>
    Cc: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Cc: Russ Anderson <rja@sgi.com>
    Cc: Yasuaki Ishimatsu <isimatu.yasuaki@jp.fujitsu.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memory_hotplug.c
+++ mm/memory_hotplug.c
@@ -1333,7 +1333,7 @@ int is_mem_section_removable(unsigned long start_pfn, unsigned long nr_pages)
 }
 
 /*
- * Confirm all pages in a range [start, end) is belongs to the same zone.
+ * Confirm all pages in a range [start, end) belong to the same zone.
  */
 int test_pages_in_a_zone(unsigned long start_pfn, unsigned long end_pfn)
 {
@@ -1344,10 +1344,11 @@ int test_pages_in_a_zone(unsigned long start_pfn, unsigned long end_pfn)
 	for (pfn = start_pfn;
 	     pfn < end_pfn;
 	     pfn += MAX_ORDER_NR_PAGES) {
-		i = 0;
-		/* This is just a CONFIG_HOLES_IN_ZONE check.*/
-		while ((i < MAX_ORDER_NR_PAGES) && !pfn_valid_within(pfn + i))
-			i++;
+		/* Find the first valid pfn in this pageblock */
+		for (i = 0; i < MAX_ORDER_NR_PAGES; i++) {
+			if (pfn_valid(pfn + i))
+				break;
+		}
 		if (i == MAX_ORDER_NR_PAGES)
 			continue;
 		page = pfn_to_page(pfn + i);
