86d54a1328b06e38a13f127abf694bcbed00dbb4
Ray Jui <rjui@broadcom.com>
2015-08-05 14:03:57 -0700

    VOIP-4918: Fix iProc MSI driver typo
    PCI: iproc: Fix typo in license header
    
    Signed-off-by: Ray Jui <rjui@broadcom.com>
    
    Bug: VOIP-4918
    Change-Id: I2a3e7acdde682ed99eb974bb398f9b4c6785067c
    Reviewed-on: http://gerrit-ccxsw.rtp.broadcom.com:8080/6995
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Reviewed-by: Ray Jui <rjui@broadcom.com>
    Tested-by: Ray Jui <rjui@broadcom.com>
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Reviewed-by: Scott Branden <sbranden@broadcom.com>

--- drivers/pci/host/pcie-iproc-msi.c
+++ drivers/pci/host/pcie-iproc-msi.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Broadcom Corporatcommon ion
+ * Copyright (C) 2015 Broadcom Corporation
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
