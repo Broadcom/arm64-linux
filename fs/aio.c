dbcde1ed6ee0a040d121d2c1777f20723918e173
Benjamin LaHaise <bcrl@kvack.org>
2016-01-10 17:58:00 -0500

    aio: enabled thread based async fsync
    Enable a fully asynchronous fsync and fdatasync operations in aio using
    the aio thread queuing mechanism.
    
    Signed-off-by: Benjamin LaHaise <ben.lahaise@solacesystems.com>
    Signed-off-by: Benjamin LaHaise <bcrl@kvack.org>

--- fs/aio.c
+++ fs/aio.c
@@ -1640,6 +1640,28 @@ ssize_t generic_async_write_iter(struct kiocb *iocb, struct iov_iter *iter)
 				     AIO_THREAD_NEED_TASK);
 }
 EXPORT_SYMBOL(generic_async_write_iter);
+
+static long aio_thread_op_fsync(struct aio_kiocb *iocb)
+{
+	return vfs_fsync(iocb->common.ki_filp, 0);
+}
+
+static long aio_thread_op_fdatasync(struct aio_kiocb *iocb)
+{
+	return vfs_fsync(iocb->common.ki_filp, 1);
+}
+
+ssize_t generic_async_fsync(struct kiocb *iocb, int datasync)
+{
+	struct aio_kiocb *req;
+
+	BUG_ON(iocb->ki_complete != aio_complete);
+	req = container_of(iocb, struct aio_kiocb, common);
+
+	return aio_thread_queue_iocb(req, datasync ? aio_thread_op_fdatasync
+						   : aio_thread_op_fsync, 0);
+}
+EXPORT_SYMBOL(generic_async_fsync);
 #endif /* IS_ENABLED(CONFIG_AIO_THREAD) */
 
 /*
@@ -1650,7 +1672,7 @@ static ssize_t aio_run_iocb(struct aio_kiocb *req, unsigned opcode,
 			    char __user *buf, size_t len, bool compat)
 {
 	struct file *file = req->common.ki_filp;
-	ssize_t ret;
+	ssize_t ret = -EINVAL;
 	int rw;
 	fmode_t mode;
 	rw_iter_op *iter_op;
@@ -1720,22 +1742,26 @@ rw_common:
 		break;
 
 	case IOCB_CMD_FDSYNC:
-		if (!file->f_op->aio_fsync)
-			return -EINVAL;
-
-		ret = file->f_op->aio_fsync(&req->common, 1);
+		if (file->f_op->aio_fsync)
+			ret = file->f_op->aio_fsync(&req->common, 1);
+#if IS_ENABLED(CONFIG_AIO_THREAD)
+		else if (file->f_op->fsync && (aio_auto_threads & 1))
+			ret = generic_async_fsync(&req->common, 1);
+#endif
 		break;
 
 	case IOCB_CMD_FSYNC:
-		if (!file->f_op->aio_fsync)
-			return -EINVAL;
-
-		ret = file->f_op->aio_fsync(&req->common, 0);
+		if (file->f_op->aio_fsync)
+			ret = file->f_op->aio_fsync(&req->common, 0);
+#if IS_ENABLED(CONFIG_AIO_THREAD)
+		else if (file->f_op->fsync && (aio_auto_threads & 1))
+			ret = generic_async_fsync(&req->common, 0);
+#endif
 		break;
 
 	default:
 		pr_debug("EINVAL: no operation provided\n");
-		return -EINVAL;
+		break;
 	}
 
 	if (ret != -EIOCBQUEUED) {
